b05d016d634ebe677cbf8604453e53817941058a
Kirtidas S. There <kirtidasthere@gmail.com>
2024-09-12 22:40:20 +0530

    09

---
+++ src/Array/LargestNumber.java
@@ -0,0 +1,50 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LargestNumber
+{
+    public static void main(String[] args) {
+        int arr[]={3,30,34,5,9};
+        //System.out.println(LargestNumber(arr));'
+        String res=LargestNumber(arr);
+        System.out.println(res);
+    }
+    public static String LargestNumber(int arr[]) {
+        Integer temp[] = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+
+        }
+        Arrays.sort(temp,new MyComparator());
+       // System.out.println(Arrays.toString(temp));
+
+        StringBuilder sb=new StringBuilder();
+        for (int x:temp)
+        {
+            sb.append(x);
+        }
+        String res=sb.toString();
+        int i=0;
+        while (i<res.length()-1 && res.charAt(i)=='0')
+        {
+            i++;
+        }
+        return res.substring(i);
+    }
+    static class MyComparator implements Comparator<Integer>
+    {
+
+        @Override
+        public int compare(Integer a, Integer b)
+        {
+            String ab=a+""+b;
+            String ba=b+""+a;
+            return ba.compareTo(ab);
+        }
+    }
+
+
+
+}
